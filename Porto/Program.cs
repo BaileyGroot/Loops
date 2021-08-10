ca84ee306c14693664c325383d9a0dc07db97041
Grootrix <grootrix@gmail.com>
2021-08-10 08:36:08 +0200

    Add project files.

---
+++ Porto/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave A
+
+            for (int a = 0; a < 100; a++)
+
+            {
+
+                if (a < 50)
+
+                {
+
+                    Console.WriteLine(a);
+
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+
+            // Opgave B
+
+
+
+            int i = 0;
+
+            while (i < 100)
+
+            {
+
+                if (i < 50)
+
+                {
+
+                    Console.WriteLine(i);
+
+                    i++;
+
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+
+          
+            // Opgave C
+
+
+
+            for (int b = 100; b > 0; b--)
+
+            {
+
+                Console.WriteLine(b);
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
